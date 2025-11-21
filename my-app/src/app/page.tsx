84caf45802e663ca42f39a426b391f555bf895d6
gyu5 <komanao700@gmail.com>
2025-11-21 17:20:20 +0900

    画面下メニューバーのコンポーネントの実装

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen text-3xl text-blue-500">
+    <div className="flex items-center justify-center h-screen text-3xl text-white-500">
       Hello LinkWallet!
     </div>
   );
