826e72d26d1c05a7c598295e8319877cd6f9bdb8
gyu5 <komanao700@gmail.com>
2025-12-06 14:47:46 +0900

    デザインの調整

--- my-app/src/app/home/page.tsx
+++ my-app/src/app/home/page.tsx
@@ -1,4 +1,5 @@
 import TravelCardCarousel from "@/components/TravelCardCarousel";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
@@ -6,6 +7,11 @@ export default function HomePage() {
       {/* 上の通知アイコンとかヘッダーはここに置く */}
       <div className="mt-10 mb-4 w-full flex justify-end pr-10 text-gray-500">
         <span className="material-symbols-outlined text-4xl!">notifications</span>
+      </div>
+      <div className="flex w-full px-4 mx-auto">
+        <div className="justify-between items-center">
+          <Image src="/logo.png" alt="LinkWallet" width={70} height={70} />
+        </div>
       </div>
 
       <TravelCardCarousel />
